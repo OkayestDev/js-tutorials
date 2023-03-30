a4bbb2dcd1d33331e70babfeb650cb71d8d2e4f3
Sir-Kyle-Richardson <richardson.a.kyle.95@gmail.com>
2023-03-30 06:43:04 -0400

    curry-fn formatting

--- src/curry/example/curried-fn.ts
+++ src/curry/example/curried-fn.ts
@@ -5,18 +5,16 @@ const partialAdd =
     (y: number): number =>
         x + y;
 
-const curriedAdd = curry((x: number, y: number) => x + y);
-
 const resultWithPartial = partialAdd(5)(4);
+
+const curriedAdd = curry((x: number, y: number) => x + y);
 
 const resultWithCurry = curriedAdd(5, 4);
 
 const curryPartial = curriedAdd(5);
-
 const resultWithPartialCurry = curryPartial(4);
 
 const partialWithPlaceHolder = curriedAdd($, 5);
-
 const resultWithPlaceholder = partialWithPlaceHolder(4);
 
 console.log({ resultWithPartial, resultWithCurry, resultWithPartialCurry, resultWithPlaceholder });
